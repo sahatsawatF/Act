f4008f78ca45ef58c77b77788b6d283eb4d74537
sahatsawatF <76928286+sahatsawatF@users.noreply.github.com>
2021-10-17 23:42:13 +0700

    initial

---
+++ test.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+       dfasdfjasdhfah
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+
+})
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
